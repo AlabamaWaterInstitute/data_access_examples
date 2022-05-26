77a56e1c03d1389d3a1571088f928fdf813bb7d7
Karnesh Jain <jain.karnesh@gmail.com>
2022-05-26 14:16:38 -0400

    started a method to get NWA data from AWS

---
+++ data.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+import s3fs
+import zarr
+import fsspec
+
+
+def get_nwm_data(feature_id, time_range):
+    """
+    Get NOAA NWM data from AWS
+    It is filtered to retrieve data for a particular time range corresponding to a particular feature ID
+
+    Arguments:
+    ----------
+    feature_id (int): Feature ID for which NWM data needs to be returned
+    time_range (array):
+
+    Returns
+    -------
+    (pandas.dataframe): Pandas dataframe with NWM data for selected time range corresponding to feature ID
+
+    """
+
+    url = "s3://noaa-nwm-retrospective-2-1-zarr-pds/chrtout.zarr"
+    fs = s3fs.S3FileSystem(anon=True)
+    store = s3fs.S3Map(url, s3=fs)
+    ds_nwm_chrtout = xr.open_zarr(store, consolidated=True)
