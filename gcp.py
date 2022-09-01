2c1ea0420eda6f5bb897b77ff2ce2ecbf49389fb
Karnesh Jain <jain.karnesh@gmail.com>
2022-09-01 16:12:58 -0400

    added property decorator for configurations

--- gcp.py
+++ gcp.py
@@ -41,3 +41,37 @@ class NWMData:
         for n in range(int((end_date - start_date).days)+1):
             yield start_date + timedelta(n)
 
+    @property
+    def configurations(self) -> list:
+        # Valid configurations compatible with this client
+        # TODO Find crosswalk for Alaska
+        return [
+            'analysis_assim',
+            'analysis_assim_extend',
+            'analysis_assim_hawaii',
+            'analysis_assim_long',
+            'analysis_assim_puertorico',
+            'long_range_mem1',
+            'long_range_mem2',
+            'long_range_mem3',
+            'long_range_mem4',
+            'medium_range_mem1',
+            'medium_range_mem2',
+            'medium_range_mem3',
+            'medium_range_mem4',
+            'medium_range_mem5',
+            'medium_range_mem6',
+            'medium_range_mem7',
+            'short_range',
+            'short_range_hawaii',
+            'short_range_puertorico',
+            'analysis_assim_no_da',
+            'analysis_assim_extend_no_da',
+            'analysis_assim_hawaii_no_da',
+            'analysis_assim_long_no_da',
+            'analysis_assim_puertorico_no_da',
+            'medium_range_no_da',
+            'short_range_hawaii_no_da',
+            'short_range_puertorico_no_da'
+        ]
+
