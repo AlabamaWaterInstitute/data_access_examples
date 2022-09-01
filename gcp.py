1e0410d264fb5b928febda3a957cf11442c6c9ba
Karnesh Jain <jain.karnesh@gmail.com>
2022-09-01 10:51:29 -0400

    added module description

---
+++ gcp.py
@@ -0,0 +1,10 @@
+"""
+
+This module provides classes that offer a convenient
+interface using kerchunk to retrieve National Water Model (NWM) data
+from Google Cloud Platform.
+
+NWM Data: https://console.cloud.google.com/marketplace/details/noaa-public/national-water-model
+
+"""
+
