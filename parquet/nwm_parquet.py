583ed282470925e6d207a25b798b2ae25acee5f5
Karnesh Jain <jain.karnesh@gmail.com>
2022-12-05 18:14:18 -0500

    modified gen_json method

--- parquet/nwm_parquet.py
+++ parquet/nwm_parquet.py
@@ -73,9 +73,6 @@ def gen_json(u, fs, outf=None):
 
     with fs.open(u, **so) as infile:
         h5chunks = SingleHdf5ToZarr(infile, u, inline_threshold=300)
-        p = u.split("/")
-        date = p[3]
-        fname = p[5]
         if outf:
             with open(outf, "wb") as f:
                 f.write(ujson.dumps(h5chunks.translate()).encode())
