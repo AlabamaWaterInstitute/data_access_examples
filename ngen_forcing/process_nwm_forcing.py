a7509f5df3d1b5863a5cfeb89469e6c0b94d9f76
James Halgren <jshalgren@ua.edu>
2022-12-22 12:17:22 -0600

    add optimized loop and port to functions

--- ngen_forcing/process_nwm_forcing.py
+++ ngen_forcing/process_nwm_forcing.py
@@ -2,11 +2,22 @@
 import xarray as xr
 import geopandas as gpd
 from rasterstats import zonal_stats
+
 # import rasterio
 import pandas as pd
 
+import time
+
+from TestConvert_NWMForcing_to_Ngen import (
+    get_forcing_dict_newway,
+    get_forcing_dict_newway_parallel,
+    get_forcing_dict_newway_inverted,
+    get_forcing_dict_newway_inverted_parallel,
+)
+
 from pathlib import Path
 import warnings
+
 warnings.simplefilter("ignore")
 
 # Read forcing files
@@ -15,7 +26,7 @@ warnings.simplefilter("ignore")
 # TODO: Add looping through lists of forcing files
 # consider looking at the "listofnwmfilenames.py" in the data_access_examples repository.
 # Integer values for runinput, varinput, etc. are listed at the top of the file
-# and an example is given in the `main` function. 
+# and an example is given in the `main` function.
 
 # import listofnwmfilenames
 #     create_file_list(
@@ -36,50 +47,118 @@ wget -P data -c https://storage.googleapis.com/national-water-model/nwm.20220824
 wget -P 03w -c https://nextgen-hydrofabric.s3.amazonaws.com/v1.2/nextgen_03W.gpkg
 """
 
-folder_prefix = Path('data')
-list_of_files = [
-    "nwm.t12z.medium_range.forcing.f001.conus.nc",
-    "nwm.t12z.medium_range.forcing.f002.conus.nc",
-    "nwm.t12z.medium_range.forcing.f003.conus.nc",
-]
 
-# Read basin boundary file
-f_03 = "03w/nextgen_03W.gpkg"
-gpkg_divides = gpd.read_file(f_03, layer="divides")
-list_of_vars = [
-    "U2D"      ,
-    "V2D"     ,
-    "LWDOWN" ,
-    "RAINRATE",
-    "T2D"   ,
-    "Q2D"   ,
-    "PSFC" ,
-    "SWDOWN",
-]
+def get_forcing_dict(
+    gpkg_divides,
+    folder_prefix,
+    filelist,
+    var_list,
+):
+    reng = "rasterio"
+    sum_stat = "mean"
 
-df_dict = {}
-for _v in list_of_vars:
-    df_dict[_v] = pd.DataFrame(index=gpkg_divides.index)
+    df_dict = {}
+    for _v in var_list:
+        df_dict[_v] = pd.DataFrame(index=gpkg_divides.index)
 
-reng = "rasterio"
-sum_stat = "mean"
+    for _nc_file in filelist:
+        # _nc_file = ("nwm.t00z.medium_range.forcing.f001.conus.nc")
+        _full_nc_file = folder_prefix.joinpath(_nc_file)
 
-for _nc_file in list_of_files:
-    # _nc_file = ("nwm.t00z.medium_range.forcing.f001.conus.nc")
-    _full_nc_file = (folder_prefix.joinpath(_nc_file))
+        with xr.open_dataset(_full_nc_file, engine=reng) as _xds:
+            for _v in var_list:
+                _src = _xds[_v]
+                _aff2 = _src.rio.transform()
+                _arr2 = _src.values[0]
 
-    with xr.open_dataset(_full_nc_file, engine=reng) as _xds:
-        for _v in list_of_vars:
-            _src = _xds[_v]
-            _aff2 = _src.rio.transform()
-            _arr2 = _src.values[0]
+                _df_zonal_stats = pd.DataFrame(
+                    zonal_stats(gpkg_divides, _arr2, affine=_aff2)
+                )
+                # if adding statistics back to original GeoDataFrame
+                # gdf3 = pd.concat([gpkg_divides, _df_zonal_stats], axis=1)
+                df_dict[_v][_xds.time.values[0]] = _df_zonal_stats[sum_stat]
 
-            breakpoint()
-            _df_zonal_stats = pd.DataFrame(zonal_stats(gpkg_divides, _arr2, affine=_aff2))
-            # if adding statistics back to original GeoDataFrame
-            # gdf3 = pd.concat([gpkg_divides, _df_zonal_stats], axis=1)
-            df_dict[_v][_xds.time.values[0]]=_df_zonal_stats[sum_stat]
+    return df_dict
+
 
 # TODO: Convert the output to CSV with something like
 # `gdf3.to_csv`
 
+
+def main():
+
+    folder_prefix = Path("data")
+    list_of_files = [
+        f"nwm.t12z.medium_range.forcing.f{_r:03}.conus.nc" for _r in range(1, 241)
+    ]
+
+    # Read basin boundary file
+    f_03 = "03w/nextgen_03W.gpkg"
+    gpkg_divides = gpd.read_file(f_03, layer="divides")
+    var_list = [
+        "U2D",
+        "V2D",
+        "LWDOWN",
+        "RAINRATE",
+        "T2D",
+        "Q2D",
+        "PSFC",
+        "SWDOWN",
+    ]
+
+    file_list = list_of_files[0:30]
+    gpkg_subset = gpkg_divides[0:2000]
+    #file_list = list_of_files[0:3]
+    #gpkg_subset = gpkg_divides[0:20]
+    feature_list = gpkg_subset.geometry.to_list()
+
+    # start_time = time.time()
+    # print(f"Working on the original way")
+    # forcing_dict = get_forcing_dict(
+    #     gpkg_subset,
+    #     folder_prefix,
+    #     file_list,
+    #     var_list,
+    #     )
+    # print(time.time() - start_time)
+
+    start_time = time.time()
+    print(f"Working on the new way")
+    fd2 = get_forcing_dict_newway(
+        feature_list,
+        folder_prefix,
+        file_list,
+    )
+    print(time.time() - start_time)
+
+    start_time = time.time()
+    print(f"Working on the new way with threading. (It's not much better.)")
+    fd3 = get_forcing_dict_newway_parallel(
+        feature_list,
+        folder_prefix,
+        file_list,
+        )
+    print(time.time() - start_time)
+
+
+    start_time = time.time()
+    print(f"Working on the new way with loops reversed.")
+    fd4 = get_forcing_dict_newway_inverted(
+        feature_list,
+        folder_prefix,
+        file_list,
+    )
+    print(time.time() - start_time)
+
+    start_time = time.time()
+    print(f"Working on the new way with loops reversed with threading.")
+    fd4 = get_forcing_dict_newway_inverted_parallel(
+        feature_list,
+        folder_prefix,
+        file_list,
+    )
+    print(time.time() - start_time)
+
+
+if __name__ == "__main__":
+    main()
