d813d7f0e196e15f0ab5590eac98bc688e37d2cb
Jordan Laser <jordan.science.laser@gmail.com>
2023-05-17 16:57:44 -0600

    Shielded subset function

--- ngen_forcing/prep_hydrofab_forcings_ngen.py
+++ ngen_forcing/prep_hydrofab_forcings_ngen.py
@@ -32,7 +32,9 @@ from listofnwmfilenamesretro import create_file_list_retro
 
 pkg_dir = Path(Path(os.path.dirname(__file__)).parent, "subsetting")
 sys.path.append(str(pkg_dir))
-from subset import subset_upstream, subset_upstream_prerelease
+from subset import subset_upstream
+if os.path.exists(pkg_dir):
+    from subset import subset_upstream_prerelease
 
 TEMPLATE_BLOB_NAME = (
     "nwm.20221001/forcing_medium_range/nwm.t00z.medium_range.forcing.f001.conus.nc"
